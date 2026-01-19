a67ea101b6718160ca7ae9e74b6e3c1d32b965b2
CrimsonRedLavender <crimsonredlavender@gmail.com>
2026-01-19 14:06:46 +0100

    code: optimized web route file

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,14 @@ use App\Http\Controllers\ReservationController;
 
 Route::get('/', function () {
     return view('welcome');
-    //return redirect('/dashboard');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']) ->middleware('verified') ->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware(['auth'])->group(function () {
 
     // Show the User's vehicle
     Route::get('/vehicles/my-vehicles', [VehicleController::class, 'index'])->name('vehicles.my');
@@ -37,11 +33,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/vehicles/{vehicle}/edit', [VehicleController::class, 'edit'])->name('vehicles.edit');
     Route::put('/vehicles/{vehicle}', [VehicleController::class, 'update'])->name('vehicles.update');
 
-
     Route::delete('/vehicles/{vehicle}', [VehicleController::class, 'destroy'])->name('vehicles.destroy');
-});
-
-Route::middleware(['auth'])->group(function () {
 
     // Show all trips of the user
     Route::get('/trips/my-trips', [TripController::class, 'index'])->name('trips.my');
@@ -50,7 +42,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/trips/create', [TripController::class, 'create'])->name('trips.create');
     Route::post('/trips', [TripController::class, 'store'])->name('trips.store');
 
-    //Search for a trip
+    // Search for a trip
     Route::get('/trips/search', [TripController::class, 'search'])->name('trips.search');
 
     // Show one trip
@@ -58,16 +50,14 @@ Route::middleware(['auth'])->group(function () {
 
     // Update a trip by cancelling it
     Route::patch('/trips/{trip}/cancel', [TripController::class, 'cancel'])->name('trips.cancel');
-});
 
-Route::middleware(['auth'])->group(function () {
+    // S'inscrire a un trajet -> créer une réservation
     Route::post('/reservations', [ReservationController::class, 'store'])
         ->name('reservations.store');
 
+    // Se désinscrire -> supprimer la réservation
     Route::delete('/reservations/{reservation}', [ReservationController::class, 'destroy'])
         ->name('reservations.destroy');
-
 });
-
 
 require __DIR__ . '/auth.php';
